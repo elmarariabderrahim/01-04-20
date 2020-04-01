7f16a1e68a34aec5e1196e8365a7a7b7b70dc5e6
elmarariabderrahim <32686758+elmarariabderrahim@users.noreply.github.com>
2020-04-01 16:02:20 +0100

    Update add_to_container.sh

--- add_to_container.sh
+++ add_to_container.sh
@@ -8,22 +8,32 @@ echo ${docker_mysql_port}
 mysql -P $docker_mysql_port --protocol=tcp -u root -ppixid123 -Bse "drop database if exists db5;create database db5;use db5;"
 
 
-input="/var/lib/jenkins/workspace/03-11-V1/logfile.txt"
-varrr=""
+input="C:\Users\Admin\Desktop\test\logfile.txt"
+var=""
 while IFS= read -r line
 do
-varrr="${varrr}$line"
+var="${var}$line"
 done < "$input"
-mysql -P $docker_mysql_port --protocol=tcp -uroot -pHGKO$.xu1234 -Bse "$varrr"
+mysql -P $docker_mysql_port --protocol=tcp -uroot -ppixid123 -Bse "$var"
+
 
 IFS=':'
 for f in sql_scripts/*; do
-input="./$f"
-varrr=""
-while IFS= read -r line
-do
-varrr="${varrr}$line"
-done < "$input"
-mysql -P $docker_mysql_port --protocol=tcp -uroot -ppixid123 -Bse "$varrr"
-done
+ input="./$f"
+ varrr=""
+   while IFS= read -r line
+     do
+     varrr="${varrr}$line"
+   done < "$input"
+ script_name=$(echo $f| cut -d'/' -f 2)
+ mysql -P $docker_mysql_port --protocol=tcp -uroot -ppixid123 -Bse "$varrr"
 
+
+ if [ "$?" -eq 0 ]; then 
+ echo "+++++"
+	mysql -uroot -ppixid123 -Bse "use db5;insert into scripts (script_name,script_state) values('$script_name','succes');"
+ else
+ echo "-----"
+	mysql -uroot -ppixid123 -Bse "use db5;insert into scripts (script_name,script_state) values('$script_name','failed');"
+ fi
+done
