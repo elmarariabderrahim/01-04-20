4801d8e8b26e6bc0b05fcb2a8882cd6ab5f2b324
elmarariabderrahim <32686758+elmarariabderrahim@users.noreply.github.com>
2020-04-09 17:21:25 +0100

    Update add_to_container.sh

--- add_to_container.sh
+++ add_to_container.sh
@@ -1,14 +1,21 @@
+	
 #!/bin/bash
+	
+results=( $( mysql --batch mysql -u root -ppixid123 -N -e "use db5; select script_name from scripts where script_state='succes';"  ) )
+
 
 str=$(docker port test-mysql)
 IFS=':'
 read -ra ADDR <<< "$str"
 docker_mysql_port=${ADDR[1]}
 echo ${docker_mysql_port}
+# acces to docker image 'test-mysql'
 mysql -P $docker_mysql_port --protocol=tcp -u root -ppixid123 
 
+path=$(pwd)
 
-input="C:\Program Files (x86)\Jenkins\workspace\pipeline_v1\logfile.txt"
+# import database schema 
+input="$path/logfile.txt"
 var=""
 while IFS= read -r line
 do
@@ -16,24 +23,58 @@ var="${var}$line"
 done < "$input"
 mysql -P $docker_mysql_port --protocol=tcp -uroot -ppixid123 -Bse "$var"
 
-
-IFS=':'
+flag=""
 for f in sql_scripts/*; do
- input="./$f"
- varrr=""
-   while IFS= read -r line
-     do
-     varrr="${varrr}$line"
-   done < "$input"
- script_name=$(echo $f| cut -d'/' -f 2)
- mysql -P $docker_mysql_port --protocol=tcp -uroot -ppixid123 -Bse "$varrr"
+	
+			script_name=$(echo $f| cut -d'/' -f 2)
+			
+			  
+				
+
+				if [[ ! ${results[*]} =~ "$script_name" ]] 
+				then
+
+					flag="0"
+					echo "$script_name n'est pas encore teste"
+					 
+				else 
+					flag="1"
+					echo "$script_name est deja teste"
+					
+				fi
+				
+
+							if [[ $flag -eq 0 ]]; then	
+					                input="./$f"
+									varrr=""	 
+									while IFS= read -r line
+									do
+									    varrr="${varrr}$line"
+									done < "$input" 
+
+									
+									mysql -P $docker_mysql_port --protocol=tcp -uroot -ppixid123 -Bse "$varrr" 
 
 
- if [ "$?" -eq 0 ]; then 
- echo " le script $script_name est passer par succes"
-	mysql -uroot -ppixid123 -Bse "use db5;insert into scripts (script_name,script_state) values('$script_name','succes');"
- else
- echo " le script ${script_name} a échoué"
-	mysql -uroot -ppixid123 -Bse "use db5;insert into scripts (script_name,script_state) values('$script_name','failed');"
- fi
+									if [ "$?" -eq 0 ]; then 
+											echo " le script $script_name est passer avec succes"
+											mysql -uroot -ppixid123 -Bse "use db5;insert into scripts (script_name,script_state) values('$script_name','succes');"
+									else
+											echo " le script ${script_name} a échoué"
+											 
+											mysql -uroot -ppixid123 -Bse "use db5;insert into scripts (script_name,script_state) values('$script_name','failed');"
+									fi 
+							else
+									echo "le script $script_name est deja tester avec succes "
+							fi
 done
+
+
+
+
+
+
+
+
+
+
