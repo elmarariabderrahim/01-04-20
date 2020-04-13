6212a39b05a91f00a72229ff229858c8fa91cf88
elmarariabderrahim <32686758+elmarariabderrahim@users.noreply.github.com>
2020-04-13 16:20:02 +0100

    Update sql1.sql

--- sql_scripts/sql1.sql
+++ sql_scripts/sql1.sql
@@ -2,7 +2,7 @@ use db5;
 insert into departments values ('1','IT');
 insert into departments values ('2','LO');
 insert into departments values ('3','EL');
-insert into departments values ('4','PH');
+insert into departments values ('4',,,'PH');
 insert into departments values ('5','RH');
 insert into departments values ('6','RS');
 insert into departments values ('7','SE');
