5d19980bc3eb52f81eee465cefc99969b2a2c7d0
elmarariabderrahim <32686758+elmarariabderrahim@users.noreply.github.com>
2020-04-13 23:02:09 +0100

    Update add_to_container.sh

--- add_to_container.sh
+++ add_to_container.sh
@@ -1,7 +1,8 @@
 	
 #!/bin/bash
 export username=$1
-export password=$2	
+export password=$2
+echo "$password"
 results=( $( mysql --batch mysql -u $username -p$password -N -e "use db5; select script_name from scripts where script_state='succes';"  ) )
 results_of_failed_scripts=( $( mysql --batch mysql -u $username -p$password -N -e "use db5; select script_name from scripts where script_state='failed';"  ) )
 
